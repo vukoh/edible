b4be97a65183a50b1c03df82318de28e9d8232fc
Sahilgat <nisgat@live.com>
2019-06-28 18:27:12 +0800

    Linked OCR parsing to searching through our database for Halal checking

--- lib/ingredient.dart
+++ lib/ingredient.dart
@@ -1,11 +1,26 @@
 class Ingredient{
+  String code;
   String name;
   String description;
+  int halal;
 
-  Ingredient(this.name, this.description);
+  Ingredient(this.code, this.name, this.description, this.halal);
 
   Ingredient.fromJson(Map<String, dynamic> json){
+    code = json['Code'];
     name = json['Name'];
     description = json['Description'];
+    halal = json['Halal'];
   }
+
+  bool isHalal() {
+    return this.halal == 1 ? true : false;
+  }
+
+/* Todo
+  bool isVegetarian() {
+
+  }
+*/
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:edible/search.dart';
+import 'package:edible/ingredient.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart'; //For repeated method
+import 'dart:convert'; //For repeated method
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
@@ -25,7 +27,9 @@ class MyHomePage extends StatelessWidget {
 
   final String title;
 
-  Future readText() async {
+  Future<List<String>> readTextfromCamera() async {
+    List<String> _stringIngredientNamesToCheck = List<String>();
+
     File pickedImage = await ImagePicker.pickImage(source: ImageSource.camera);
     FirebaseVisionImage ourImage = FirebaseVisionImage.fromFile(pickedImage);
     TextRecognizer recognizeText = FirebaseVision.instance.textRecognizer();
@@ -34,12 +38,55 @@ class MyHomePage extends StatelessWidget {
     for (TextBlock block in readText.blocks) {
       for (TextLine line in block.lines) {
         for (TextElement word in line.elements) {
-          print(word.text);
+          _stringIngredientNamesToCheck.add(word.text);
         }
       }
     }
+    return _stringIngredientNamesToCheck;
   }
-  //Change function name
+  //Todo: Fix function so using back in camera doesn't crash app
+
+  Future<List<Ingredient>> fetchIngredient() async {
+    var url = 'https://cryptic-lake-93970.herokuapp.com/ingredients';
+    var response = await get(url);
+
+    var ingredients = List<Ingredient>();
+  	
+    if (response.statusCode == 200) {
+      var ingredientsJson = json.decode(response.body);
+      for (var ingredientJson in ingredientsJson) {
+        ingredients.add(Ingredient.fromJson(ingredientJson));
+      }
+    }
+
+    return ingredients;
+  }
+  //Repeated function from search.dart, how to abstract this away but mounted/not mounted issue
+  
+  void finalFunction() async {
+    var _stringIngredientNamesToCheck = await readTextfromCamera();
+    var ingredients = await fetchIngredient();
+    bool _foundIngredient = false;
+
+    for(String name_to_check in _stringIngredientNamesToCheck) {
+      _foundIngredient = false;
+      for(Ingredient curr_ingredient in ingredients) {
+        if(curr_ingredient.name == name_to_check) {
+          _foundIngredient = true;
+          print('Found ingredient: ' + name_to_check);
+          if(curr_ingredient.isHalal()) {
+            print(name_to_check + ' is halal');
+          } else {
+            print(name_to_check + ' is not halal');
+          }
+        }
+      }
+      if(_foundIngredient == false) {
+        print(name_to_check + ' not found');
+      }
+    }
+  }
+  //Rename to something better, better architecture possible?
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +115,7 @@ class MyHomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.camera_alt),
-        onPressed: readText,
+        onPressed: finalFunction,
       ),
     );
   }
